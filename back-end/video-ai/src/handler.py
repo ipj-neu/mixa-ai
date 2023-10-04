72df2eeb9bfc0d3cd686b0975ee8df586f4ba0fc
ipj-neu <ijohnson@student.neumont.edu>
2023-10-04 13:20:39 -0600

    I mean half of a chatgpt clone is done lol

--- back-end/video-ai/src/handler.py
+++ back-end/video-ai/src/handler.py
@@ -1,12 +1,66 @@
 import json
+import os
+
+import boto3
+
+from langchain.memory.chat_message_histories import DynamoDBChatMessageHistory
+from langchain.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import LLMChain
+
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
-def hello(event, context):
-    print("Body: ", json.loads(event["body"]))
-    print("here")
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
-    }
+def testing(event: dict[str, any], context: dict[str, any]):
+    os.environ["OPENAI_API_KEY"] = "APIKEY"
+    stage = "dev"  # need to change to an env
 
-    return {"statusCode": 200, "body": json.dumps(body)}
+    body = json.loads(event.get("body", {}))
+    message = body.get("message")
+    session = body.get("session")
+    table_name = f"video-ai-{stage}-chat-sessions"
+
+    dynamodb = boto3.resource("dynamodb")
+
+    table = dynamodb.Table(table_name)
+
+    response = table.get_item(Key={"SessionId": session})
+
+    if "Item" not in response:
+        table.put_item(
+            Item={
+                "SessionId": session,
+                "History": [],
+            }
+        )
+
+    history = DynamoDBChatMessageHistory(table_name=table_name, session_id=session)
+    memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=history, return_messages=True)
+
+    llm = ChatOpenAI()
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "Your name is Steveo and you are a quirky guy that loves to have funny conversations. You are known to always make puns, but you have some anger issues.",
+            ),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+
+    llm_chain = LLMChain(llm=llm, memory=memory, prompt=prompt)
+
+    logger.info(f"Running with message: {message}")
+    result = llm_chain.run({"input": message})
+    logger.info(f"Result: {result}")
+
+    return {"statusCode": 200, "body": json.dumps({"message": result})}
+
+
+def on_message(event, context):
+    pass
