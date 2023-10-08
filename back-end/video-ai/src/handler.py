84f2ee71ece56f2a8132222d5a3078885ad2b7f1
ipj-neu <ijohnson@student.neumont.edu>
2023-10-07 22:43:06 -0600

    added a lot of notes and did A LOT of research. started on creating the agent that will be used.

--- back-end/video-ai/src/handler.py
+++ back-end/video-ai/src/handler.py
@@ -6,47 +6,100 @@ except ImportError:
 
 import json
 import os
+from typing import Dict
 
 import boto3
+from pprint import pprint
 
 from langchain.memory.chat_message_histories import DynamoDBChatMessageHistory
 from langchain.memory import ConversationBufferMemory
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.chains import LLMChain
+
+from langchain.agents import OpenAIFunctionsAgent
+from langchain.tools import Tool
+from langchain.agents import AgentExecutor
+from langchain.tools.render import format_tool_to_openai_function
 
 import logging
+
+from src.media_convert import JobSettings, JobCreationInput
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def testing_new(event: dict[str, any], context: dict[str, any]) -> dict[str, any]:
+def testing_tool(input_settings: Dict) -> str:
+    setting = JobSettings(**input_settings)
+    print(type(setting))
+    return f"I'll make that for you! It could take a second..."
+
+
+def handle_message(event, context):
     stage = os.environ.get("STAGE", "dev")
     logger.info(f"Running in stage: {stage}")
 
+    # this is all code for http stuff and will be moved when moved to step function
     body = json.loads(event.get("body", {}))
     message = body.get("message")
     session = body.get("session")
     table_name = f"video-ai-{stage}-chat-sessions"
+    user_id = event["requestContext"]["authorizer"]["jwt"]["claims"]["sub"]
 
+    # this process of checking if the table exists and creating it should not need to happen later in development
     dynamodb = boto3.resource("dynamodb")
 
     table = dynamodb.Table(table_name)
 
-    response = table.get_item(Key={"SessionId": session})
+    response = table.get_item(Key={"sessionId": session, "userId": user_id})
 
     if "Item" not in response:
         table.put_item(
             Item={
-                "SessionId": session,
+                "sessionId": session,
+                "userId": user_id,
                 "History": [],
             }
         )
 
-    history = DynamoDBChatMessageHistory(table_name=table_name, session_id=session)
+    history = DynamoDBChatMessageHistory(
+        table_name=table_name, session_id=session, key={"sessionId": session, "userId": user_id}
+    )
     memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=history, return_messages=True)
 
+    llm = ChatOpenAI()
+    tools = [
+        Tool.from_function(
+            name="create_job",
+            func=testing_tool,
+            description="testing tool",
+            args_schema=JobCreationInput,
+            return_direct=True,
+        )
+    ]
+
+    agent: OpenAIFunctionsAgent = OpenAIFunctionsAgent.from_llm_and_tools(
+        llm=llm, tools=tools, extra_prompt_messages=[MessagesPlaceholder(variable_name="chat_history")]
+    )
+
+    executor = AgentExecutor.from_agent_and_tools(
+        agent=agent,
+        tools=tools,
+        memory=memory,
+        verbose=True,
+    )
+    message = executor.run({"input": message})
+
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps({"message": message}),
+    }
+
+
+def blank_lambda(event: dict[str, any], context) -> dict[str, any]:
+    pprint(event)
+    pprint(context)
     return {
         "statusCode": 500,
         "headers": {"Content-Type": "application/json"},
@@ -54,53 +107,52 @@ def testing_new(event: dict[str, any], context: dict[str, any]) -> dict[str, any
     }
 
 
-def testing(event: dict[str, any], context: dict[str, any]) -> dict[str, any]:
+def testing(event: dict[str, any], context) -> dict[str, any]:
     stage = os.environ.get("STAGE", "dev")
     logger.info(f"Running in stage: {stage}")
 
+    # this is all code for http stuff and will be moved when moved to step function
     body = json.loads(event.get("body", {}))
     message = body.get("message")
     session = body.get("session")
     table_name = f"video-ai-{stage}-chat-sessions"
-
-    # this process of checking if the table exists and creating it should not need to happen later in development
-    dynamodb = boto3.resource("dynamodb")
-
-    table = dynamodb.Table(table_name)
-
-    response = table.get_item(Key={"SessionId": session})
-
-    if "Item" not in response:
-        table.put_item(
-            Item={
-                "SessionId": session,
-                "History": [],
-            }
-        )
-
-    history = DynamoDBChatMessageHistory(table_name=table_name, session_id=session)
-    memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=history, return_messages=True)
+    user_id = event["requestContext"]["authorizer"]["jwt"]["claims"]["sub"]
 
     llm = ChatOpenAI()
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                "Your name is Steveo and you are a quirky guy that loves to have funny conversations. You are known to always make puns, but you have some anger issues.",
-            ),
-            MessagesPlaceholder(variable_name="chat_history"),
-            ("human", "{input}"),
-        ]
+    tools = [
+        Tool.from_function(
+            name="create_job",
+            func=testing_tool,
+            description="testing tool",
+            args_schema=JobCreationInput,
+            return_direct=True,
+        )
+    ]
+
+    agent: OpenAIFunctionsAgent = OpenAIFunctionsAgent.from_llm_and_tools(llm=llm, tools=tools)
+
+    executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+    message = executor.run(
+        {
+            "input": "create a job from ThisVid.mp4 from 0:00:00 to 0:00:10 then create a different job for OtherVid.mp4 with timestamps 0:00:30 to 0:00:40"
+        }
     )
 
-    llm_chain = LLMChain(llm=llm, memory=memory, prompt=prompt)
-
-    logger.info(f"Running with message: {message}")
-    result = llm_chain.run({"input": message})
-    logger.info(f"Result: {result}")
-
-    return {"statusCode": 200, "headers": {"Content-Type": "application/json"}, "body": json.dumps({"message": result})}
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps({"message": "This is a test function"}),
+    }
 
 
-def on_message(event, context):
-    pass
+if __name__ == "__main__":
+    tools = [
+        Tool.from_function(
+            name="create_job",
+            func=testing_tool,
+            description="testing tool",
+            args_schema=JobCreationInput,
+            return_direct=True,
+        )
+    ]
+    print(format_tool_to_openai_function(tools[0]))
--- back-end/video-ai/src/media_convert.py
+++ back-end/video-ai/src/media_convert.py
@@ -29,6 +29,14 @@ class Input(BaseModel):
     video_name: str = Field(..., description="The name of the input video")
 
 
+class JobSettings(BaseModel):
+    inputs: Sequence[Input] = Field(..., description="The inputs of the job")
+
+
+class JobCreationInput(BaseModel):
+    setting: JobSettings = Field(..., description="The settings of the job")
+
+
 """
 the ai need to determine whether it needs to ask clarifying questions or not
 first step determine if the request needs any clarifying questions
@@ -46,5 +54,33 @@ create an agent with the correct tools
 create a runnable map that will run the chain will feed to route the request to the agent or to the clarifying question
     the map will have the agent and a lambda that will set the message to be returned to the user
 
-well fuck they have exactly what i need
+will use the conversational react agent to do this
+conversational agent prompt can be modified to tell it when to ask clarifying questions
+    need to look into what open ai function agent does, potentially has better output than the conversational agent and maybe more control
+looking into the code, the open ai function agent does what i want
+    it can use multi input tools and is a lot more customizable
+    the conversation part of asking follow up question can be done with a tool that will send the message to the user and memory
+tools have a field to stop the agent and return
+the ai can call for the rek stuff if not available, the step function will call the job creation then call the agent lambda again.
+    should probably not store it and create embeddings
+        a stretch goal could be to create a vector database of rek results that the ai can query
+        might not be that much of an improvement over just formatting the rek results and sending it to the tool
+        the rek tool should be an ai that the formatted data gets sent to and processed into something the agent can turn into a convert job
+
+the step function could be triggered by http directly or by a lambda that is triggered by http
+    the lambda would probably be better because it can do some error checking, formatting, and auth before sending it to the step function
+does the new messages need to be added to a queue to be processed by the step function?
+    this might be unnecessary complexity because the step function can scale
+step function:
+needs the message sent by the user, the session id (chat id), and the user id (maybe more info about the user)
+first it needs to determine where the request came from (lambda or the recall from the rek job)
+if call is directly from the user, send to the agent the agent lambda
+    - the agent can send it back to the user if it needs clarifying questions (conditions to ask clarifying questions can be added to the prompt)
+    - request the rek info if it needs it and if not available creates the job and sends it back to the agent lambda when rek job is finished
+        need to figure out how to set the waiting for completion and calling the agent lambda again
+        needs to send updates to the user about what its doing so the user doesn't just sit there waiting
+    - create a convert job from all available info and send it to the queue
+if the call is from the rek job it can do all of the things above except it needs to remove the previous user message and ask it again given the new info from the rek job
+    unless it keeps track of it's thought process and can just be told to continue from where it left off
+    would need to remove the continue message from the db history
 """
