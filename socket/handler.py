054eed5e39d1d929e62995965d95f555ceb6cbf7
ipj-neu <ijohnson@student.neumont.edu>
2023-10-17 22:15:55 -0600

    lambda to notify user of status change on the table for the step function. still need to finallize how that flow will work

--- socket/handler.py
+++ socket/handler.py
@@ -30,39 +30,3 @@ def on_disconnect(event, context):
         print(e)
 
     return {"statusCode": 200}
-
-
-def send_message(event, context):
-    # HACK this function needs to be changed to be used in the step function
-    #   DO NOT FOCUS ON THIS NOT A PRIORITY RIGHT NOW
-    """This function is made to be used in te step function to send users updates"""
-    print("send_message")
-    stage = os.getenv("STAGE")
-    domain_name = os.getenv("DOMAIN_NAME")
-    region = os.getenv("REGION")
-    print(type(domain_name))
-
-    # TODO figure out how i want to get the user id and data in the step function
-    userId = ""
-    data = {}
-
-    table_name = f"video-ai-{stage}-connections"
-    table = boto3.resource("dynamodb").Table(table_name)
-
-    connections = table.query(KeyConditionExpression=boto3.dynamodb.conditions.Key("userId").eq(userId))
-
-    url = f"https://{domain_name}.execute-api.{region}.amazonaws.com/{stage}"
-    print(url)
-    client = boto3.client(
-        "apigatewaymanagementapi",
-        endpoint_url=url,
-    )
-
-    for connection in connections["Items"]:
-        try:
-            client.post_to_connection(ConnectionId=connection["connectionId"], Data=data)
-        except client.exceptions.GoneException:
-            connections.delete_item(Key={"connectionId": connection["connectionId"], "userId": userId})
-            print("connection does not exist: " + connection["connectionId"])
-
-    return {"statusCode": 200}
