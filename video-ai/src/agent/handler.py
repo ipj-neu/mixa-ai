0db08a544c8b2f040784b8f7a806c1369ba1da51
ipj-neu <ijohnson@student.neumont.edu>
2023-10-25 21:16:44 -0600

    added logic rerun the ai from the fetch_rek

--- video-ai/src/agent/handler.py
+++ video-ai/src/agent/handler.py
@@ -4,22 +4,31 @@ try:
 except ImportError:
     pass
 
-import json
 import os
 from typing import Dict, Any, List
 import boto3
 import logging
-from media_convert import ToolInput, Input
 
 from langchain.memory import ConversationBufferMemory, DynamoDBChatMessageHistory
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import MessagesPlaceholder
+from langchain.prompts.chat import MessagesPlaceholder, SystemMessagePromptTemplate
 from langchain.agents import OpenAIFunctionsAgent
-from langchain.tools import Tool
+from langchain.tools import BaseTool
 from langchain.agents import AgentExecutor
 
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
+
+
+class TestingTool(BaseTool):
+    name = "testing_tool"
+    description = "testing tool do not use"
+
+    def _run(self, *args: Any, **kwargs: Any) -> Any:
+        raise NotImplementedError
+
+    def _arun(self, *args: Any, **kwargs: Any) -> Any:
+        raise NotImplementedError
 
 
 def video_agent(event: Dict[str, Any], context) -> Dict[str, Any]:
@@ -33,69 +42,42 @@ def video_agent(event: Dict[str, Any], context) -> Dict[str, Any]:
     origin = event["origin"]
 
     # NOTE better ways to get the table name
-    table_name = f"video-ai-{stage}-chat-sessions"
-
-    if origin == "fetch_rek":
-        session_table = boto3.resource("dynamodb").Table(table_name)
-        response = session_table.update_item(
-            Key={"sessionId": session, "userId": user_id},
-            UpdateExpression="REMOVE History[-1], History[-1]",
-            ReturnValues="UPDATED_OLD",
-        )
-        # sets the message the user sent as the message to be processed before getting the data
-        message = response["Attributes"]["History"][0]
-
-
-# TODO rewrite to fit in the step function
-def video_agent_old(event: Dict[str, Any], context) -> Dict[str, Any]:
-    stage = os.environ.get("STAGE", "dev")
-    logger.info(f"starting handle_message video-ai-{stage}")
-
-    # all information should always be there except for maybe the message depending on the origin
-    user_id = event["userId"]
-    session = event["sessionId"]
-    message = event.get("message", None)
-    origin = event["origin"]
-
-    # NOTE better ways to get the table name
-    table_name = f"video-ai-{stage}-chat-sessions"
-
-    if origin == "process_rek":
-        # TODO pop the most recent message from the db, and set it as the message
-        #   kinda a hacky way to do it but it should work unless I make custom memory, maybe?
-        session_table = boto3.resource("dynamodb").Table(table_name)
-        pass
-
-    # TODO needs to be made into a tool that inherits from BaseTool and moved to another file
-    # will allow conditional return_direct and passing the tool variables from the function instead of relying on scope
-    def testing_tool(input: str) -> str:
-        return "no information available"
+    table_name = f"video-ai-{stage}-chat-sessions-history"
 
     history = DynamoDBChatMessageHistory(
         table_name=table_name, session_id=session, key={"sessionId": session, "userId": user_id}
     )
-    memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=history, return_messages=True)
 
-    llm = ChatOpenAI()
-    tools = [
-        Tool.from_function(
-            name="testing_tool",
-            func=testing_tool,
-            description="should not be used for anything. exists to test the tool system",
-            return_direct=True,
+    # NOTE might need to add more origins, this is the only one rn that needs to be handled differently
+    if origin == "fetch_rek":
+        message_history = history.messages
+        session_table = boto3.resource("dynamodb").Table(table_name)
+        response = session_table.update_item(
+            Key={"sessionId": session, "userId": user_id},
+            UpdateExpression=f"REMOVE History[{len(message_history) - 2}], History[{len(message_history) - 1}]",
+            ReturnValues="UPDATED_OLD",
         )
-    ]
+        message = response["Attributes"]["History"][0]["data"]["content"]
+    logger.info(f"message: {message}")
 
-    agent: OpenAIFunctionsAgent = OpenAIFunctionsAgent.from_llm_and_tools(
-        llm=llm, tools=tools, extra_prompt_messages=[MessagesPlaceholder(variable_name="chat_history")]
+    memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=history, return_messages=True)
+    # TODO set temp of ai. want it to be low enough to use the tools correctly, but high enough create good videos.
+    llm = ChatOpenAI()
+    # TODO define tools in other file
+    tools = [TestingTool()]
+
+    # TODO create better prompts for the agent
+    #   system prompt needs work
+    agent = OpenAIFunctionsAgent.from_llm_and_tools(
+        llm,
+        tools,
+        system_message=SystemMessagePromptTemplate.from_template(
+            "You are a helpful AI assistant that is amazing at editing videos."
+        ),
+        extra_prompt_messages=[MessagesPlaceholder(variable_name="chat_history")],
     )
 
-    executor = AgentExecutor.from_agent_and_tools(
-        agent=agent,
-        tools=tools,
-        memory=memory,
-        verbose=True,
-    )
-    message = executor.run({"input": message})
+    executor = AgentExecutor.from_agent_and_tools(agent, tools, memory=memory, verbose=True)
 
-    # TODO add return to direct the next step in the step function
+    output = executor.run({"input": message})
+    logger.info(f"agent final output: {output}")
