5547578fc5b416a8269b06092c3c35645e22e77a
ipj-neu <ijohnson@student.neumont.edu>
2023-11-13 22:23:38 -0700

    sockets should be mostly finished need to test sending messages to users

--- socket/handler.py
+++ socket/handler.py
@@ -6,7 +6,7 @@ def on_connect(event, context):
     print("on_connect")
     stage = os.getenv("STAGE")
     user_id = event["requestContext"]["authorizer"]["principalId"]
-    table_name = f"video-ai-{stage}-connections"
+    table_name = f"video-ai-{stage}-user-connections"
 
     table = boto3.resource("dynamodb").Table(table_name)
     table.put_item(Item={"connectionId": event["requestContext"]["connectionId"], "userId": user_id})
@@ -17,16 +17,11 @@ def on_connect(event, context):
 def on_disconnect(event, context):
     print("on_disconnect")
     stage = os.getenv("STAGE")
-    table_name = f"video-ai-{stage}-connections"
+    table_name = f"video-ai-{stage}-user-connections"
 
     table = boto3.resource("dynamodb").Table(table_name)
     connectionId = event["requestContext"]["connectionId"]
 
-    try:
-        connections = table.query(KeyConditionExpression=boto3.dynamodb.conditions.Key("connectionId").eq(connectionId))
-        for item in connections["Items"]:
-            table.delete_item(Key={"connectionId": connectionId, "userId": item["userId"]})
-    except Exception as e:
-        print(e)
+    table.delete_item(Key={"connectionId": connectionId})
 
     return {"statusCode": 200}
